d579060d3f89b6768f5de561e2da4b8975a04c84
Alexander Prechtel <aprechtel0043@gmail.com>
2025-01-24 22:41:04 -0500

    User Login
    2:06:22 - 2:22:27 Django Tutorial

--- Django Tutorial/studybud/base/views.py
+++ Django Tutorial/studybud/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 # Create your views here.
@@ -22,8 +23,15 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    topic = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topic, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
