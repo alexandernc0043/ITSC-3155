5bf120e045b3b7a72421decc3685e856c0cbd081
Alexander Prechtel <aprechtel0043@gmail.com>
2025-01-25 21:47:11 -0500

    Restricted Pages
    2:39:03 - 2:41:45 Django Tutorial

--- Django Tutorial/studybud/base/views.py
+++ Django Tutorial/studybud/base/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
-
+from django.contrib import messages
 
 # Create your views here.
 
@@ -23,14 +24,26 @@ from django.contrib.auth.models import User
 # ]
 def loginPage(request):
     if request.method == "POST":
-        username = request.POST.get('name')
+        username = request.POST.get('username')
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
         except:
-            
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
     context = {}
     return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
